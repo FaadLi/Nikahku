79dd04d670d385661b227ed471ca997dbf92b498
FaadLi <mzfadlii@gmail.com>
2019-11-10 21:52:01 +0700

    kategori

--- application/controllers/CHome.php
+++ application/controllers/CHome.php
@@ -3,9 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CHome extends CI_Controller {
 
+		public function __construct()
+	{
+		parent::__construct();
+
+		// Load semua model yang dipakai
+		$this->load->model('kategori_model');
+	}
+
 	public function index()
 	{
-		$this->load->view('Home');
-		// $this->load->view('Kategori');
+		// $this->load->view('Home');
+		$this->load->view('Kategori');
+	}
+	public function kategori() 
+	{
+
+		// Dapatkan semua kategori
+		$data['categories'] = $this->kategori_model->get_all_dekorasi();
+
+
+		$this->load->view('Kategori', $data);
+
 	}
 }
--- application/views/Kategori.php
+++ application/views/Kategori.php
@@ -12,36 +12,119 @@
         
 		<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
 		<script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
-
+        <script src="<?php echo base_url() ?>assets/js/holder.js"></script>
+        <!-- Online -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<!-- Custom -->
 		<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/custom/home.css">
 		
+
+        <style>
+           
+            * {
+            box-sizing: border-box;
+            }
+
+            form.example input[type=text] {
+            padding: 10px;
+            font-size: 17px;
+            border: 1px solid grey;
+            float: left;
+            width: 80%;
+            background: #f1f1f1;
+            }
+
+            form.example button {
+            float: left;
+            width: 20%;
+            padding: 10px;
+            background: #2196F3;
+            color: white;
+            font-size: 17px;
+            border: 1px solid grey;
+            border-left: none;
+            cursor: pointer;
+            }
+
+            form.example button:hover {
+            background: #0b7dda;
+            }
+
+            form.example::after {
+            content: "";
+            clear: both;
+            display: table;
+            }
+            
+            a:link, a:visited {
+            background-color: white;
+            color: black;
+            padding: 14px 25px;
+            text-align: left;
+            text-decoration: none;
+            display: inline-block;
+            }
+
+            a:hover, a:active {
+             background-color: #f2f2f2; */
+            }
+        </style>
+
 	</head>
 
-
-	<body style="background: #ff9999; font-size: 14px;">
-
-
- 
- 
-        <footer class="jumbotron footer">
-            <div class="row">
-                <div class="col">
-                    
-                    Tentang Kami
-                    
-                </div>  
-                <div class="col">
-                Tentang Kita
-                </div> 
-                <div class="col">
-                Tentang Semua
-                </div>
+	<body style="background:#ff8 ; font-size: 14px;">
+<!-- #ff9999 -->
+    <div  >
+        <div class="" style="background: #fff; ">
+        <br><br><br>
+            <h1 class="text-center"> Ini Kategori yang dicari </h1>
+            <div class="search-container">
+                <form class="example" action="/action_page.php" style="margin:auto;max-width:800px">
+                    <input type="text" placeholder="Search.." name="search2">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
             </div>
-            <hr>
-            <p><span class="glyphicon glyphicon-copyright-mark">Wedding Organizer </span></p>
-        </footer>
+            <hr><hr>
+        </div>
+        
 
+        <div class="container">
+        <div class="row text-center">
+        <?php
+            // Kita looping data dari controller
+            foreach ($categories as $key) :
+        ?>
+        <div class="container col-md-4" >
+        <br><br>
+            <div class="card-deck ">
+                <a href="#" class="card mb-4 box-shadow border-0">
+                    <img class="card-img-top" src="holder.js/200x150?auto=yes&text=foto <?php echo ($key->nama) ?>" alt="<?php echo ($key->nama) ?>">
+                    
+                    <div class="card-body">
+                    
+                        <h4 class="card-title"><?php echo ($key->nama) ?></h4>
+                        <hr>
+                        <p class="card-text"><?php echo ($key->keterangan) ?></p>
+                    </div>
+                    <div class="card-footer text-right">
+                        <small class="text-muted "><?php echo ($key->tampstamp) ?></small>
+                    </div>
+                </a> 
+            </div>
+        </div>
+        <?php endforeach; ?>
+        </div>
+        </div>
+
+    </div>
+    <div>
+        
+
+    </div>
+    
+    
+ 
+        
  
  
         <!-- Bootstrap JavaScript -->
